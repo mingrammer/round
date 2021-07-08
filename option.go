61d276e82f25419498ab6d819bd14b33989f4a1f
HeCorr <hccaseclone@gmail.com>
2021-07-08 19:09:32 -0300

    modify parseOptions() to handle '-b' flag (base64 mode)

--- option.go
+++ option.go
@@ -40,6 +40,7 @@ func parseOptions() *option {
 	rate := flag.Float64("r", opts.rate, "rounding rate. 1 means circular. (0~1)")
 	corner := flag.String("c", "tl,tr,bl,br", "comma separated corners to round.")
 	owrite := flag.Bool("w", false, "if true, will overwrite the original files.")
+	base64 := flag.Bool("b", false, "if true, will read base64-encoded bytes from stdin.")
 	output := flag.String("o", opts.output, "output file name for a single file.")
 	prefix := flag.String("p", opts.prefix, "prefix for the output file names.")
 	suffix := flag.String("s", opts.suffix, "suffix for the output file names.")
@@ -54,6 +55,7 @@ func parseOptions() *option {
 		log.Fatalln(err)
 	}
 	opts.owrite = *owrite
+	opts.base64 = *base64
 	opts.output = *output
 	opts.prefix = *prefix
 	opts.suffix = *suffix
